2bde2fa1afa6104e9723c3fc2da8b850f8e4b87c
Admin <hungln006@gmail.com>
2022-01-19 09:56:24 +0700

    hi

--- src/component-elements/LikeCommentShare.vue
+++ src/component-elements/LikeCommentShare.vue
@@ -2,19 +2,19 @@
    <GridLayout dock="top" columns="*, *, *" style="padding: 20px 0;">
             <!-- Like -->
             <GridLayout col="0"  columns="*,auto, *" @tap="likeHandle" width="33%">
-                <Image src="res://like2" row="0" column="0" class="postIcon" stretch="aspectFill" />
+                <Image src="res://like2" row="0" column="0" class="iconlcs" stretch="aspectFill" />
                 <Label text="Thích" row="0" col="1" textWrap="true" class="postlcs"
                     :class="likeBoolean ? 'liked' : ''" />
                     <!-- :style="{ color: likeBoolean ? 'blue' : 'black' }" /> -->
             </GridLayout>
             <!-- Comment -->
             <GridLayout col="1"  columns="*, auto, *" width="33%">
-                <Image src="res://comment" row="0" column="0" class="postIcon" stretch="aspectFill" />
+                <Image src="res://comment" row="0" column="0" class="iconlcs" stretch="aspectFill" />
                 <Label text="Bình luận" row="0" col="1" textWrap="true" class="postlcs" />
             </GridLayout>
             <!-- Share -->
             <GridLayout col="2"  columns="*, auto, *" width="33%">
-                <Image src="res://share" row="0" column="0" class="postIcon" stretch="aspectFill" />
+                <Image src="res://share" row="0" column="0" class="iconlcs" stretch="aspectFill" />
                 <Label text="Chia sẻ" row="0" col="1" textWrap="true" class="postlcs" />
             </GridLayout>
     </GridLayout>
@@ -34,5 +34,7 @@ export default {
     .liked {
         color: rgb(72, 89, 245); font-weight: bold;
     }
-   
+    .iconlcs {
+        width: 80px; height: 80px; border-radius: 360px;
+    }
 </style>
